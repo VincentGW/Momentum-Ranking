a0bfd3e5f8b5eac6f385c8a183918ae576c4c8e6
Vincent Wisehoon <52048662+VincentGW@users.noreply.github.com>
2019-09-20 11:52:03 -0400

    Add files via upload

--- excelmanip.py
+++ excelmanip.py
@@ -6,12 +6,18 @@ import datetime
 import os
 
 thisyear = str(datetime.datetime.now().year)
-thismonth = str(datetime.datetime.now().month)
-thisday = str(datetime.datetime.now().day)
+lastyear = str(datetime.datetime.now().year - 1)
 otheryear = str(datetime.datetime.now().year - 2)
+thismonth = str(datetime.datetime.now().month)
+lastmonth = str(datetime.datetime.now().month - 1)
+thisday = str(datetime.datetime.now().day)
+datedone = False
 
 if len(thismonth) == 1:
     thismonth = '0'+thismonth
+
+if len(lastmonth) == 1:
+    lastmonth = '0'+lastmonth
 
 if len(thisday) == 1:
     thisday = '0'+thisday
@@ -21,6 +27,7 @@ datefolder = "%s-%s-%s"%(thisyear,thismonth,thisday)
 excel_file = xw.Book('MomentumCalc.xlsx')
 excel_file_dos = xw.Book('MomentumRank.xlsm')
 sort_macro = excel_file_dos.macro('Sort')
+average_macro = excel_file_dos.macro('Averages')
 f_calc = excel_file.sheets['MomentumCalc']
 f_rank = excel_file_dos.sheets['MomentumRank']
 
@@ -41,12 +48,13 @@ def chuck_csv_data(arg):
         print('Copying data point no. '+ str(data.index(x)+1) +' to MomentumCalc.xlsx in Excel...', end='\r')
         f_calc.range(myrange).value = x
 
-def transfer(num, ticker):
-    num += 1
+def transfer(num, ticker, momentum):
+    num += 2
     tickerloc = 'B'+str(num)
     r90loc = 'C'+str(num)
     r125loc = 'D'+str(num)
     r250loc = 'E'+str(num)
+    genericloc = 'G'+str(num)
     adjslope90 = f_calc.range('H509').value
     adjslope125 = f_calc.range('L509').value
     adjslope250 = f_calc.range('P509').value
@@ -54,7 +62,25 @@ def transfer(num, ticker):
     f_rank.range(r90loc).value = adjslope90
     f_rank.range(r125loc).value = adjslope125
     f_rank.range(r250loc).value = adjslope250
+    f_rank.range(genericloc).value = momentum
     sort_macro()
+
+def import_date(tickerarg):
+    global datedone
+    if datedone == False:
+        filename = tickerarg+"-"+datefolder
+        with open(os.getcwd()+r"/"+filename+".csv",'r') as f_from:
+            readCSV = csv.reader(f_from, delimiter=',')
+            datelist = []
+            for row in readCSV:
+                datelist.append(row[0])
+        for x in datelist:
+            myrange = 'B'+str((datelist.index(x)+1))
+            print('Creating date '+ str(datelist.index(x)+1) +' in MomentumCalc.xlsx in Excel...', end='\r')
+            f_calc.range(myrange).value = x
+        datedone = True
+    else:
+        pass
 
 def load_series(stock):
     chuck_csv_data(manifest_pricelist(stock))
--- run.py
+++ run.py
@@ -5,25 +5,26 @@
 #    Libraries used include xlwings for handling the excel templates in which analysis is done
 #    and BeautifulSoup for handling requests sent out to retrieve stock data
 #    To learn more about the concept behind the Momentum Ranking technique and how it can aid in
-#    securities trading, it is recommended you read Andreas Clenow's book: "Stocks on the Move" (2015)
+#    securities trading, you should read Andreas Clenow's book: "Stocks on the Move" (2015)
 
 import os
 import json
 import csv
+import pickle
+import time
 import requests
 import bs4
 import excelmanip
-from excelmanip import datefolder, thismonth, thisday, thisyear, otheryear
+from excelmanip import datefolder, thismonth, thisday, thisyear, lastmonth, lastyear, otheryear
 from bs4 import BeautifulSoup as soup
+
+list_of_stocks = []
 
 if not os.path.exists(datefolder):
     print('Creating a new folder for today\'s ranking...')
     os.makedirs(datefolder)
 
-list_of_stocks = []
-
-def get_sp500():
-    global list_of_stocks
+def get_sp500_to_pickle():
     print('Gathering up-to-date list of S&P500 stock tickers...')
     wikiurl = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     scrape = soup(wikiurl.text, "lxml")
@@ -31,42 +32,110 @@ def get_sp500():
     stocks = []
     for row in table.findAll('tr')[1:]:
         stock = row.findAll('td')[0].text
-        stocks.append(stock)
-    
+        stocks.append(stock)    
     stocks = list(map(lambda s: s.strip(), stocks))
-    print('Stocks successfully acquired')
-    for x in stocks:
-        list_of_stocks.append(x)
+    with open("sp500tickers.pickle",'wb') as f:
+        pickle.dump(stocks, f)
+    print('Tickers saved to binary file offline...')
 
-def savePrices(arg):
-    print('Downloading two years of stock history for the company ' + arg + '...')
-    ticker = arg
-    koyfin_url = 'https://api.koyfin.com/api/v2/commands/g/g.eq/%s?dateFrom=%s-%s-%s&dateTo=%s-%s-%s&period=daily'%(ticker,otheryear,thismonth,thisday,thisyear,thismonth,thisday)
-    r = requests.get(koyfin_url)
-    scrape = soup(r.text, "html.parser")
-    priceDictionary = json.loads(str(scrape))
-    series = priceDictionary["graph"]["data"]
-    iterable = []
-    for i in range(0, len(series)):
-        stup = (series[i][0], series[i][4])
-        iterable.append(stup)
+def load_tickers_to_list():
+    global list_of_stocks
+    with open("sp500tickers.pickle",'rb') as f:
+        list_of_stocks = pickle.load(f)
 
+def get_prices_to_pickle():
     if os.path.basename(os.getcwd()) != datefolder:
         os.chdir(os.getcwd()+"\\"+datefolder)
     else:
         pass
-    myfile = '%s-%s.csv'%(ticker,datefolder)
-    f = open(myfile,'w',newline='')
-    writer = csv.writer(f)
-    for tup in iterable:
-        writer.writerow(tup)
-    f.close
+    temp_dict = {}
+    for ticker in list_of_stocks:
+        print('Downloading two years of stock history for company ' + ticker + '...')
+        time.sleep(.100)
+        koyfin_url = 'https://api.koyfin.com/api/v2/commands/g/g.eq/%s?dateFrom=%s-%s-%s&dateTo=%s-%s-%s&period=daily'%(ticker,otheryear,thismonth,thisday,thisyear,thismonth,thisday)
+        r = requests.get(koyfin_url)
+        scrape = soup(r.text, "html.parser")
+        priceDictionary = json.loads(str(scrape))
+        table = priceDictionary["graph"]["data"]
+        singleseries = []
+        for i in range(0, len(table)):
+            stup = (table[i][0], table[i][4])
+            singleseries.append(stup)
+        temp_dict.update({ticker:singleseries})
+    with open("Series_Database.pickle",'wb') as f:
+        pickle.dump(temp_dict,f)
+    print('Stock history database saved to binary file offline...')
+
+def twelve_month_pickle():
+    if os.path.basename(os.getcwd()) != datefolder:
+        os.chdir(os.getcwd()+"\\"+datefolder)
+    else:
+        pass
+    momentumdict = {}
+    for ticker in list_of_stocks:
+        print('Downloading monthly stock history for company ' + ticker + '...')
+        time.sleep(.100)
+        koyfin_url = 'https://api.koyfin.com/api/v2/commands/g/g.eq/%s?dateFrom=%s-%s-%s&dateTo=%s-%s-%s&period=monthly'%(ticker,lastyear,lastmonth,thisday,thisyear,thismonth,thisday)
+        r = requests.get(koyfin_url)
+        scrape = soup(r.text, "html.parser")
+        priceDictionary = json.loads(str(scrape))
+        table = priceDictionary["graph"]["data"]
+        singleseries = []
+        for i in range(0, len(table)):
+            stup = table[i][4]
+            singleseries.append(stup)
+        returnseries = []
+        for i in range(0, len(singleseries) - 1):
+            chain1 = int(i)
+            chain2 = int(i + 1)
+            x = (singleseries[chain2]-singleseries[chain1])/(singleseries[chain1])
+            x += 1
+            returnseries.append(x)
+        gross = 1
+        for n in returnseries:
+            gross = gross * n
+        momentum = format(gross, "2%")
+        momentumdict[ticker] = momentum
+    with open("Generic_Momentum_Database.pickle",'wb') as f:
+        pickle.dump(momentumdict,f)
+    print('Twelve month momentum database saved to binary file offline...')
+
+def load_database_to_csv():
+    if os.path.basename(os.getcwd()) != datefolder:
+        os.chdir(os.getcwd()+"\\"+datefolder)
+    else:
+        pass
+    with open("Series_Database.pickle",'rb') as f:
+        series_database = pickle.load(f)
+    for (key, values) in series_database.items():
+        print('Writing data from binary file to csv for company ' + key + '...')
+        myfile = '%s-%s.csv'%(key,datefolder)
+        f = open(myfile,'w',newline='')
+        writer = csv.writer(f)
+        for value in values:
+            writer.writerow(value)
+        f.close
 
 def main():
-    get_sp500()
+    print('Did you already download stock data today? y/n')
+    if input() == 'n':
+        get_sp500_to_pickle()
+        load_tickers_to_list()
+        get_prices_to_pickle()
+        load_database_to_csv()
+        twelve_month_pickle()
+        pass
+    else:
+        load_tickers_to_list()
+        os.chdir(os.getcwd()+"\\"+datefolder)
+        pass
+        excelmanip.average_macro()
+    with open("Generic_Momentum_Database.pickle",'rb') as f:
+        momentumdict = pickle.load(f)
     for stock in list_of_stocks:
-        savePrices(stock)
+        excelmanip.import_date(stock)
         excelmanip.load_series(stock)
-        excelmanip.transfer(list_of_stocks.index(stock),stock)
+        excelmanip.transfer(list_of_stocks.index(stock),stock,momentumdict[stock])
+    excelmanip.average_macro()
 
 main()
